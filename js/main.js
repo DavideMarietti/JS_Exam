4fa2151a1bcfa23b77a79d93cc45e06236aa63bf
DavideMarietti <76477890+DavideMarietti@users.noreply.github.com>
2022-12-28 17:33:12 +0100

    Everything works fine.

--- js/main.js
+++ js/main.js
@@ -5,20 +5,14 @@ import {createApp} from "./load-data.js";
 createApp()
     .then((app) => {
 
-        //console.log(app)
-
         const userButtons = document.querySelector(".user-buttons");
 
         const allUsersName = Object.keys(app.users);
         const firstUser = allUsersName[0];
         let user = app.users[firstUser];
 
-        const userButtonsText = allUsersName.map(name => (name === firstUser ?
-            `<p user-name="${name}" class="nav__el nav__el--cta selected">${name}</p>`
-            : `<p user-name="${name}" class="nav__el nav__el--cta">${name}</p>`));
-
+        const userButtonsText = allUsersName.map(name => `<p user-name="${name}" class="nav__el nav__el--cta">${name}</p>`);
         userButtons.innerHTML = userButtonsText.join("");
-
         const mainNavButtons = userButtons.querySelectorAll("p");
 
         for (const p of mainNavButtons) {
@@ -30,19 +24,107 @@ createApp()
                 for (const child of all) {
                     if (child !== p) child.classList.remove("selected")
                 }
-                reset();
+                init_profile(user);
             });
         }
 
-        function reset() {
+
+        function init_profile(user) {
+            const home = document.querySelector(".home");
+            home.style.display = 'none';
+            const nav = document.querySelector(".second-nav");
+            nav.style.display = 'flex';
+
+            const navButtons = document.querySelector(".nav-buttons");
+            const navButton = navButtons.querySelectorAll("li");
+
+            let i = 0
+            while (i < navButton.length) {
+                if (i == 0) {
+                    navButton[i].classList.add("selected");
+                } else {
+                    navButton[i].classList.remove("selected")
+                }
+                i++
+            }
+
+            profilo(user)
+
+            for (const b of navButton) {
+                if (b.innerHTML === "Profilo utente") {
+                    b.addEventListener("click", () => {
+                        const all = b.parentElement.children;
+                        for (const child of all) {
+                            if (child !== b) {
+                                child.classList.remove("selected")
+                            } else {
+                                child.classList.add("selected")
+                            }
+                        }
+                        profilo(user);
+                    })
+                } else if (b.innerHTML === "Collezione attuale") {
+                    b.addEventListener("click", () => {
+                        const all = b.parentElement.children;
+                        for (const child of all) {
+                            if (child !== b) {
+                                child.classList.remove("selected")
+                            } else {
+                                child.classList.add("selected")
+                            }
+                        }
+                        collezione_attuale(user);
+                    })
+                } else if (b.innerHTML === "Piante disponibili") {
+                    b.addEventListener("click", () => {
+                        const all = b.parentElement.children;
+                        for (const child of all) {
+                            if (child !== b) {
+                                child.classList.remove("selected")
+                            } else {
+                                child.classList.add("selected")
+                            }
+                        }
+                        piante_disponibili(user);
+                    })
+                } else if (b.innerHTML === "Piante prestate") {
+                    b.addEventListener("click", () => {
+                        const all = b.parentElement.children;
+                        for (const child of all) {
+                            if (child !== b) {
+                                child.classList.remove("selected")
+                            } else {
+                                child.classList.add("selected")
+                            }
+                        }
+                        piante_prestate(user);
+                    })
+                }
+            }
+        }
+
+        function profilo(user) {
+            const main = document.querySelector(".collection");
+            main.style.display = 'none';
+        }
+
+        function collezione_attuale(user) {
+
+            const home = document.querySelector(".home");
+            home.style.display = 'none';
+            const main = document.querySelector(".collection");
+            main.style.display = 'flex';
+            const nav = document.querySelector(".second-nav");
+            nav.style.display = 'flex';
+
             const title = document.querySelector(".collection-title");
             title.innerText = "Collezione di " + user.name;
+
             const list = document.querySelector(".collection-content");
             list.innerHTML = "";
 
             for (let coll_item of user.currentCollection) {
 
-                //console.log(coll_item)
                 let state = "";
 
                 if (!coll_item.available) {
@@ -60,12 +142,10 @@ createApp()
                         <div class="card__picture">
                           <img src='../img/plants/${coll_item.collectable.img}' alt="plant img" class="card__picture-img">
                         </div>
-            
                         <h3 class="heading-tertirary">
                           <span>${coll_item.collectable.name}</span>
                         </h3>
                       </div>
-            
                       <div class="card__details">
                         <div class="card__data">
                           <svg class="card__icon">
@@ -93,10 +173,6 @@ createApp()
                         </div>
                         <div class="card__data"></div>
                       </div>
-            
-                       <div class="item-details">
-                        <a href="#" class="btn btn--green btn--small">Details</a>
-                       </div>
                       <div class="card__footer">
                         <p class="card__sub-heading">${state}</p>
                       </div>
@@ -114,5 +190,165 @@ createApp()
             }
         }
 
-        reset();
+        function piante_disponibili(user) {
+            const home = document.querySelector(".home");
+            home.style.display = 'none';
+            const main = document.querySelector(".collection");
+            main.style.display = 'flex';
+            const nav = document.querySelector(".second-nav");
+            nav.style.display = 'flex';
+
+            const title = document.querySelector(".collection-title");
+            title.innerText = "Piante disponibili";
+
+            const list = document.querySelector(".collection-content");
+            list.innerHTML = "";
+
+            for (let u of allUsersName) {
+
+                const collectUser = app.users[u];
+
+                if (collectUser.name !== user.name && collectUser.constructor.name === "Collector") {
+                    for (let coll_item of collectUser.availableItems) {
+
+                        let state = "disponibile da " + u;
+
+                        const template = `<div class="card-item">
+                      <div class="card__header">
+                        <div class="card__picture">
+                          <img src='../img/plants/${coll_item.collectable.img}' alt="plant img" class="card__picture-img">
+                        </div>
+                        <h3 class="heading-tertirary">
+                          <span>${coll_item.collectable.name}</span>
+                        </h3>
+                      </div>
+                      <div class="card__details">
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.group}</span>
+                        </div>
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.origin}</span>
+                        </div>
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.sun}</span>
+                        </div>
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.water}</span>
+                        </div>
+                        <div class="card__data"></div>
+                      </div>
+                       <div class="item-details">
+                        <a href="#" class="btn btn--green btn--small loan-btn" owner="${u}">Prendi in prestito</a>
+                       </div>
+                      <div class="card__footer">
+                        <p class="card__sub-heading">${state}</p>
+                      </div>
+                    </div>`
+
+                        list.insertAdjacentHTML("beforeend", template);
+                    }
+                } else continue
+            }
+
+            const allitems = list.querySelectorAll(".card-item");
+
+            for (let item of allitems) {
+                item.addEventListener("mousedown", () => {
+                    item.classList.toggle("evidenzia");
+                });
+            }
+
+            const allButtons = document.querySelectorAll(".loan-btn");
+            for (const p of allButtons) {
+                console.log(p)
+            }
+        }
+
+        function piante_prestate(user) {
+            const home = document.querySelector(".home");
+            home.style.display = 'none';
+            const main = document.querySelector(".collection");
+            main.style.display = 'flex';
+            const nav = document.querySelector(".second-nav");
+            nav.style.display = 'flex';
+
+            const title = document.querySelector(".collection-title");
+            title.innerText = "Piante prestate";
+
+            const list = document.querySelector(".collection-content");
+            list.innerHTML = "";
+
+            for (let coll_item of user.onLoanItems) {
+
+                if (coll_item.owner === user) {
+                    let state = "in prestito a " + coll_item.holder.name;
+
+                    const template = `<div class="card-item">
+                      <div class="card__header">
+                        <div class="card__picture">
+                          <img src='../img/plants/${coll_item.collectable.img}' alt="plant img" class="card__picture-img">
+                        </div>
+                        <h3 class="heading-tertirary">
+                          <span>${coll_item.collectable.name}</span>
+                        </h3>
+                      </div>
+                      <div class="card__details">
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.group}</span>
+                        </div>
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.origin}</span>
+                        </div>
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.sun}</span>
+                        </div>
+                        <div class="card__data">
+                          <svg class="card__icon">
+                            <use xlink:href="../img/icons.svg#icon-map-pin"></use>
+                          </svg>
+                          <span>${coll_item.collectable.water}</span>
+                        </div>
+                        <div class="card__data"></div>
+                      </div>
+                       <div class="item-details">
+                        <a href="#" class="btn btn--green btn--small">Riprendi</a>
+                       </div>
+                      <div class="card__footer">
+                        <p class="card__sub-heading">${state}</p>
+                      </div>
+                    </div>`
+
+                    list.insertAdjacentHTML("beforeend", template);
+                }
+            }
+
+            const allitems = list.querySelectorAll("li");
+
+            for (let item of allitems) {
+                item.addEventListener("mousedown", () => {
+                    item.classList.toggle("evidenzia");
+                });
+            }
+        }
     });
